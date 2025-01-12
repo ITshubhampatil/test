730c5f90ab049a4e2909bd93d5f74f4df2af5559
LAPTOP-UNQFR8NP\subup <subupatil83@gmail.com>
2025-01-12 18:28:12 +0530

    adding default

---
+++ demo/Employee.java
@@ -0,0 +1,38 @@
+
+public class Employee {
+
+	private int salary;
+
+	private String name;
+	private String lastname;
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [salary=" + salary + ", name=" + name + ", lastname=" + lastname + "]";
+	}
+
+}
